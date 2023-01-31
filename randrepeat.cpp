cbed849572e533ac192f37da7eb3c5436acd4c85
Gergely Szigeti on MacBook Air <szigetigergo@gmail.com>
2023-01-31 13:35:32 +0100

    added range and index percent info

--- randrepeat.cpp
+++ randrepeat.cpp
@@ -6,9 +6,15 @@ int main()
     constexpr unsigned int init_state = 123456789;
     MyRand myrand(init_state);
     int count = 0;
-    std::cout << "Repeated at indices:\n";
+    auto max_index = static_cast<unsigned int>(-1);
+    std::cout << "Repeated at indices (between 1 and "
+              << max_index << "):\n";
     for (unsigned int i = 1; i; i++) {
-        if (myrand() == init_state) { std::cout << i << "\n"; count++; }
+        if (myrand() == init_state) {
+            std::cout << i << " ("
+                      << static_cast<float>(i)/max_index*100 << "%)\n";
+            count++;
+        }
     }
     std::cout << "\nRepeated " << count << " time"
               << (count != 1 ? "s" : "") << ".\n";
