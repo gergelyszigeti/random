42a269e909e2af2a0b313915af3d8b108cc76574
Gergely Szigeti on MacBook Air <szigetigergo@gmail.com>
2023-01-31 17:13:38 +0100

    fix human readable numbers, leading zeros added

--- humanreadable.cpp
+++ humanreadable.cpp
@@ -3,7 +3,12 @@
 std::string humanReadable(unsigned int n, char separator) {
         std::string human_readable;
         while(n) {
-            human_readable = (n < 1000 ? "" : std::string(1,separator))
+            size_t leading_zeros =   static_cast<size_t>(n % 1000 < 100)
+                                   + static_cast<size_t>(n % 1000 <  10);
+            human_readable = (n < 1000 ? ""
+                               : std::string(1,separator)
+                                 + std::string(leading_zeros,'0')
+                             )
                              + std::to_string(n % 1000)
                              + human_readable;
             n /= 1000;
