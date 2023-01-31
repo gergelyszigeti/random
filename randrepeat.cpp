33d1875b15bd41700fd8444dedfa0fa145648f5b
Gergely Szigeti on MacBook Air <szigetigergo@gmail.com>
2023-01-31 21:44:37 +0100

    more verbose printouts

--- randrepeat.cpp
+++ randrepeat.cpp
@@ -8,13 +8,16 @@ int main()
     MyRand myrand(init_state);
     int count = 0;
     auto max_index = static_cast<unsigned int>(-1);
-    std::cout << "Repeated at indices (between 1 and "
+    std::cout << "Initial number repeated at indices (between 1 and "
               << humanReadable(max_index) << "):\n";
     for (unsigned int i = 1; i; i++) {
         if (myrand() == init_state) {
             std::cout << humanReadable(i) << " ("
-                      << static_cast<float>(i)/max_index*100 << "%)\n";
+                      << static_cast<float>(i)/max_index*100 << "%)";
             count++;
+            if (count == 1)
+               { std::cout << " <-- also the length of unique sequence"; }
+            std::cout << "\n";
         }
     }
     std::cout << "\nRepeated " << count << " time"
