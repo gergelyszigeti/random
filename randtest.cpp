6cda3b8c5c4f50f7cd8214b625884046bf9e61a0
Gergely Szigeti on MacBook Air <szigetigergo@gmail.com>
2023-02-01 11:54:57 +0100

    modified way of random throws
    now the fun begins:
    I renamed 'lot' to 'bucket' and we have an aim now:
    each bucket must be filled with a given amount of random results.
    
    See how interesting the histogram starts to look
    with just a few throws in each bucket.
    It is quite ok, as the number of throws increases,
    the uniformity improves.

--- randtest.cpp
+++ randtest.cpp
@@ -1,6 +1,7 @@
 // this is a little stupid histogram for testing uniformity of custom rand functions
 
 #include <iostream>
+#include <iomanip>
 #include <array>
 
 #include "myrand.h"
@@ -9,31 +10,33 @@ int main()
 {
    MyRand myrand;
 
-   printf("RAND_MAX is %d\n", RAND_MAX); 
-   
    auto rand_uniform = [&myrand]{
                    return static_cast<float>(myrand() & RAND_MAX)/RAND_MAX; };
 
-   constexpr int dice_throw_count = 1e8;
-   constexpr int lot_count = 80;
-   auto lot = std::array<int, lot_count + 1>();
-   
-   // create and write out a histogram of millions of random throws with lot_count resolution
+   constexpr int bucket_target = 100;
+   constexpr int bucket_count = 80;
+   constexpr int dice_throw_count = bucket_target * bucket_count;
+   auto buckets = std::array<int, bucket_count + 1>();
+
+   std::cout << "Target quantity in each bucket: " << bucket_target << "\n";
+
+   // create and write out a histogram of random throws with bucket_count resolution
    for (int i = 0; i < dice_throw_count; ++i) {
-       ++lot[static_cast<int>(rand_uniform()*lot_count)];
+       ++buckets[static_cast<int>(rand_uniform()*bucket_count)];
    }
    // write out the values
-   for (int i = 0; i <= lot_count; ++i) {
-       std::cout << lot[i] << ((i+1)%10? ", " : "\n");
+   for (int i = 0; i <= bucket_count; ++i) {
+       std::cout << std::setw(6) << buckets[i]
+                 << (((i+1)%10 && i != bucket_count) ? ", " : "\n");
    }
    // show bars
    std::cout << "\nBars:\n";
    constexpr int barheight = 10;
-   float max_bar = *std::max_element(std::begin(lot), std::end(lot));
+   float max_bar = *std::max_element(std::begin(buckets), std::end(buckets));
    float div = (max_bar+1) / (barheight+1);
    int y = barheight; while (y --> 0) {
-       for(int x = 0; x < lot_count; x++) {
-           std::cout << (lot[x] > static_cast<int>(y * div) ? "O" : " ");
+       for(int x = 0; x < bucket_count; x++) {
+           std::cout << (buckets[x] > static_cast<int>(y * div) ? "O" : " ");
        }
        std::cout << "\n";
    }
